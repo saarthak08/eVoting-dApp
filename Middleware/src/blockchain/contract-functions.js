926fbf010bbfe8878d621621b606fa6031823831
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-01-03 23:07:27 +0530

    middleware: feat: add more blockchain-functions

---
+++ Middleware/src/blockchain/contract-functions.js
@@ -0,0 +1,84 @@
+const { web3 } = require("../../utils/web3");
+const eVoting = require("../../utils/eVoting");
+const { unlockAccount } = require("./web3-functions");
+
+const getTotalVotesCount = async () => {
+	return await eVoting.methods.totalVotesCount().call();
+};
+
+const getTotalVotersCount = async () => {
+	return await eVoting.methods.totalVotersCount().call();
+};
+
+const getTotalCandidatesCount = async () => {
+	return await eVoting.methods.totalCandidatesCount().call();
+};
+
+const getFeesForCandidates = async () => {
+	return await eVoting.methods.feesForCandidates().call();
+};
+
+const getFeesForVoters = async () => {
+	return await eVoting.methods.feesForVoters().call();
+};
+
+const getCandidates = async () => {
+	return await eVoting.methods.getCandidates().call();
+};
+
+const isVoter = async (address) => {
+	return await eVoting.methods.totalVotersList(address).call();
+};
+
+const isCandidate = async (address) => {
+	return await eVoting.methods.candidatesMap(address).call();
+};
+
+const hasVoted = async (address) => {
+	return await eVoting.methods.votedList(address).call();
+};
+
+const getElectionCommissioner = async () => {
+	return await eVoting.methods.electionCommissioner().call();
+};
+
+const registerAsVoter = async (address, password) => {
+	await unlockAccount(address, password);
+	return await eVoting.methods
+		.registerAsVoter()
+		.send({ from: address, gas: "3000000" });
+};
+
+const registerAsCandidate = async (name, party, address, password) => {
+	await unlockAccount(address, password);
+	return await eVoting.methods.registerAsCandidate(name, party).send({
+		from: address,
+		value: "50000000000000000",
+		gas: "3000000",
+	});
+};
+
+const vote = async (voterAddress, candidatesAddress, index, password) => {
+	await unlockAccount(address, password);
+	return await eVoting.methods.vote(candidatesAddress, index).send({
+		from: voterAddress,
+		value: "900000000000000000",
+		gas: "3000000",
+	});
+};
+
+module.exports = {
+	getTotalVotersCount,
+	getCandidates,
+	getElectionCommissioner,
+	getFeesForCandidates,
+	getFeesForVoters,
+	getTotalCandidatesCount,
+	getTotalVotesCount,
+	isCandidate,
+	isVoter,
+	registerAsCandidate,
+	registerAsVoter,
+	vote,
+	hasVoted,
+};
--- Middleware/src/blockchain/web3-functions.js
+++ Middleware/src/blockchain/web3-functions.js
@@ -1,18 +1,23 @@
-const {web3,fetchDeployerAccount} = require('../../utils/web3');
+const { web3, fetchDeployerAccount } = require("../../utils/web3");
 
 const addAccount = async (password) => {
-    const account = await web3.eth.personal.newAccount(password);
-    const deployerAccount = await fetchDeployerAccount();
-    console.log(deployerAccount);
+	const account = await web3.eth.personal.newAccount(password);
+	const deployerAccount = await fetchDeployerAccount();
+	await unlockAccount(deployerAccount, process.env.ACCOUNT_PASSWORD);
 	return await web3.eth.sendTransaction(
-			{
-				from: deployerAccount,
-				to: account,
-				gas: "3000000",
-				value: "1000000000000000000",
-				data: "",
-			},
-			process.env.ACCOUNT_PASSWORD
-		);
-}
+		{
+			from: deployerAccount,
+			to: account,
+			gas: "3000000",
+			value: "1000000000000000000",
+			data: "",
+		},
+		process.env.ACCOUNT_PASSWORD
+	);
+};
 
+const unlockAccount = async (address, password) => {
+	await web3.eth.personal.unlockAccount(address, password, 36000);
+};
+
+module.exports = { addAccount, unlockAccount };
