21cdb2f143de82f6d267480ba15c996fc84d60c4
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-01-03 20:16:14 +0530

    middleware: add simple test

---
+++ Middleware/test/contract.test.js
@@ -0,0 +1,27 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const {
+	contractABI,
+	contractBytecodeObject,
+} = require("../utils/contract-utils");
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let eVoting;
+
+beforeEach(async () => {
+	accounts = await new web3.eth.getAccounts();
+
+	eVoting = await new web3.eth.Contract(contractABI)
+		.deploy({
+			data: contractBytecodeObject,
+		})
+		.send({ gas: 3000000, from: accounts[0] });
+});
+
+describe("eVoting Contract", () => {
+	it("deploys a contract", () => {
+		assert.ok(eVoting.options.address);
+	});
+});
