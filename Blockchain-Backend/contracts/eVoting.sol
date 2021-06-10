6c2f4b4411bdc428121f19c536f4414830c95e8c
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-06-10 14:17:08 +0530

    fix: lower the cost of voting

--- Blockchain-Backend/contracts/eVoting.sol
+++ Blockchain-Backend/contracts/eVoting.sol
@@ -1,76 +1,79 @@
-pragma solidity >=0.5 <0.8;
+pragma solidity >=0.5.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 
 contract eVoting {
-    
     struct Candidate {
         address accountAddress;
         address[] voters;
-        uint votersCount;
+        uint256 votersCount;
     }
-    
-    address public electionCommissioner;
-    
-    mapping(address=>bool) public totalVotersList;
-    mapping(address=>bool) public votedList;
-    
-    Candidate[] candidatesList;
-    mapping(address=>bool) public candidatesMap;
-    
-    uint public feesForCandidates;
-    uint public feesForVoters;
-    uint public totalVotersCount;
-    uint public totalVotesCount;
-    uint public totalCandidatesCount;
 
-     
+    address public electionCommissioner;
+
+    mapping(address => bool) public totalVotersList;
+    mapping(address => bool) public votedList;
+
+    Candidate[] candidatesList;
+    mapping(address => bool) public candidatesMap;
+
+    uint256 public feesForCandidates;
+    uint256 public feesForVoters;
+    uint256 public totalVotersCount;
+    uint256 public totalVotesCount;
+    uint256 public totalCandidatesCount;
+
     modifier restrictVoting() {
-        require(msg.sender.balance!=0,"Insufficient Balance");
-        require(totalVotersList[msg.sender],"Not a Voter");
-        require(!votedList[msg.sender],"Already Voted");
-        require(msg.value == 0.9 ether,"Insufficient Balance to Vote");
+        require(msg.sender.balance != 0, "Insufficient Balance");
+        require(totalVotersList[msg.sender], "Not a Voter");
+        require(!votedList[msg.sender], "Already Voted");
+        require(msg.value == 0.8 ether, "Insufficient Balance to Vote");
         _;
     }
-    
+
     constructor() {
-        electionCommissioner=msg.sender;
-        feesForCandidates=0.05 ether;
-        feesForVoters=0.9 ether;
+        electionCommissioner = msg.sender;
+        feesForCandidates = 0.05 ether;
+        feesForVoters = 0.8 ether;
     }
-    
-    
+
     function registerAsCandidate() public payable {
-        require(msg.value==feesForCandidates,"Wrong Fees Provided!");
-        require(!candidatesMap[msg.sender],"Already a Candidate");
+        require(msg.value == feesForCandidates, "Wrong Fees Provided!");
+        require(!candidatesMap[msg.sender], "Already a Candidate");
         address[] memory voters;
-        Candidate memory candidate=Candidate({
-            accountAddress:msg.sender,
-            voters:voters,
-            votersCount:0
-        });
+        Candidate memory candidate =
+            Candidate({
+                accountAddress: msg.sender,
+                voters: voters,
+                votersCount: 0
+            });
         totalCandidatesCount++;
         candidatesList.push(candidate);
-        candidatesMap[msg.sender]=true;
+        candidatesMap[msg.sender] = true;
     }
-    
+
     function registerAsVoter() public {
-        require(!totalVotersList[msg.sender],"Already Registered Voter!");
+        require(!totalVotersList[msg.sender], "Already Registered Voter!");
         totalVotersCount++;
-        totalVotersList[msg.sender]=true;
+        totalVotersList[msg.sender] = true;
     }
-    
-    function getCandidates() public view returns(Candidate[] memory) {
+
+    function getCandidates() public view returns (Candidate[] memory) {
         return candidatesList;
     }
-    
-   
-    function vote(address candidate, uint index) public payable restrictVoting {
-        require(candidatesMap[candidate],"Not a Valid Candidate Given");
-        require(candidatesList[index].accountAddress==candidate,"Candidate's address & index don't match");
+
+    function vote(address candidate, uint256 index)
+        public
+        payable
+        restrictVoting
+    {
+        require(candidatesMap[candidate], "Not a Valid Candidate Given");
+        require(
+            candidatesList[index].accountAddress == candidate,
+            "Candidate's address & index don't match"
+        );
         candidatesList[index].votersCount++;
         candidatesList[index].voters.push(msg.sender);
         totalVotesCount++;
-        votedList[msg.sender]=true;
+        votedList[msg.sender] = true;
     }
-    
 }
